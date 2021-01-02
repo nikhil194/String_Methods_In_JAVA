c7580db48f0c9a023220cc3dc6b26b28af3cf637
nikhil194 <74468247+nikhil194@users.noreply.github.com>
2021-01-02 22:36:30 +0530

    Update String_replace.java

--- String_replace.java
+++ String_replace.java
@@ -2,7 +2,7 @@ String buffer replace
 public class Stringl_replace {
     public static void main(String args[]){
         StringBuffer s=new StringBuffer("Hello");
-        s.replace(0,3,"mr.khajuriya");
+        s.replace(0,3,"mr.dwivedi");
         System.out.println(s);
 }
 }
